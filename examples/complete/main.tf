5edc8a01db6d2b2154d851c50dad9e5eaaf5b7c5
nitrocode <nitrocode@users.noreply.github.com>
2021-11-16 19:34:40 -0600

    Add fifo_queue_enabled

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -7,9 +7,9 @@ module "sns" {
 
   allowed_aws_services_for_sns_published = var.allowed_aws_services_for_sns_published
 
-  sqs_dlq_enabled = true
-  fifo_topic      = true
-  fifo_queue      = true
+  sqs_dlq_enabled    = true
+  fifo_topic         = true
+  fifo_queue_enabled = true
 
   context = module.this.context
 }
