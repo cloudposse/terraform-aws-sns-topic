23f05a9ceaf294a40f98e8ca9182fec361e2e3db
nitrocode <nitrocode@users.noreply.github.com>
2021-11-16 19:53:05 -0600

    Added periods and return list of one

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,31 @@
 output "sns_topic" {
-  value       = local.enabled ? aws_sns_topic.this : null
-  description = "SNS topic"
+  value       = local.enabled ? aws_sns_topic.this[0] : null
+  description = "SNS topic."
 }
 
 output "sns_topic_name" {
   value       = local.enabled ? local.sqs_queue_name : null
-  description = "SNS topic name"
+  description = "SNS topic name."
 }
 
 output "sns_topic_id" {
   value       = local.enabled ? aws_sns_topic.this[0].id : null
-  description = "SNS topic ID"
+  description = "SNS topic ID."
 }
 
 output "sns_topic_arn" {
   value       = local.enabled ? aws_sns_topic.this[0].arn : null
-  description = "SNS topic ARN"
+  description = "SNS topic ARN."
 }
 
 output "sns_topic_owner" {
   value       = local.enabled ? aws_sns_topic.this[0].owner : null
-  description = "SNS topic owner"
+  description = "SNS topic owner."
 }
 
 output "aws_sns_topic_subscriptions" {
-  value       = local.enabled ? aws_sns_topic_subscription.this : null
-  description = "SNS topic subscription"
+  value       = local.enabled ? aws_sns_topic_subscription.this[0] : null
+  description = "SNS topic subscription."
 }
 
 output "dead_letter_queue_url" {
@@ -44,6 +44,6 @@ output "dead_letter_queue_name" {
 }
 
 output "dead_letter_queue_arn" {
-  description = "The ARN of the dead letter queue"
+  description = "The ARN of the dead letter queue."
   value       = local.sqs_dlq_enabled ? aws_sqs_queue.dead_letter_queue[0].arn : null
 }
