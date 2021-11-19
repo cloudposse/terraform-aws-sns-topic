fbb9e74c24164b111367ff60c7850d7dbeff7d0d
nitrocode <nitrocode@users.noreply.github.com>
2021-11-19 15:43:28 -0600

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -8,8 +8,8 @@ module "sns" {
   allowed_aws_services_for_sns_published = var.allowed_aws_services_for_sns_published
 
   sqs_dlq_enabled    = false
-  fifo_topic         = true
-  fifo_queue_enabled = true
+  fifo_topic         = false
+  fifo_queue_enabled = false
 
   context = module.this.context
 }
