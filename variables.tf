4f587fd1d270c81e994a94689642a0c8658df1a6
Volodymyr Hetmanshchuk <vladget@gmail.com>
2024-10-09 14:50:56 +0300

    Added filter_policy and filter_policy_scope functionality to subscriber

--- variables.tf
+++ variables.tf
@@ -4,9 +4,13 @@ variable "subscribers" {
     # The protocol to use. The possible values for this are: sqs, sms, lambda, application. (http or https are partially supported, see below) (email is an option but is unsupported, see below).
     endpoint = string
     # The endpoint to send data to, the contents will vary with the protocol. (see below for more information)
-    endpoint_auto_confirms = bool
+    endpoint_auto_confirms = option(bool, false)
     # Boolean indicating whether the end point is capable of auto confirming subscription e.g., PagerDuty (default is false)
-    raw_message_delivery = bool
+    filter_policy = optional(string, null)
+    # The filter policy JSON that is assigned to the subscription. For more information, see Amazon SNS Filter Policies.
+    filter_policy_scope = optional(string, "MessageAttributes")
+    # The filter policy scope that is assigned to the subscription. Whether the `filter_policy` applies to `MessageAttributes` (default) or `MessageBody`
+    raw_message_delivery = optional(bool, false)
     # Boolean indicating whether or not to enable raw message delivery (the original message is directly passed, not wrapped in JSON with the original message in the message property) (default is false)
   }))
   description = "Required configuration for subscibres to SNS topic."
